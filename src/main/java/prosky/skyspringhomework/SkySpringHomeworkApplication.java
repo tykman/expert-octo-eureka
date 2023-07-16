dda1b4e2f6120db0e071f72e2f0ddc4445363582
tykman <nizom.ra@inbox.ru>
2023-07-16 15:49:38 +0500

    первый коммит

---
+++ src/main/java/prosky/skyspringhomework/SkySpringHomeworkApplication.java
@@ -0,0 +1,13 @@
+package prosky.skyspringhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkySpringHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkySpringHomeworkApplication.class, args);
+	}
+
+}
