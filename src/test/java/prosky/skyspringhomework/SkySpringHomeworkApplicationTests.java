1f4c0c66df15894fadc016431d38a0100091447b
tykman <nizom.ra@inbox.ru>
2023-07-22 21:54:20 +0500

    первый коммит

---
+++ src/test/java/prosky/skyspringhomework/SkySpringHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package prosky.skyspringhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkySpringHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
